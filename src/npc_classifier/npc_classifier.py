437a1700f109ef045d2be63db81af3bd2f778106
Yuichi Tateno <hotchpotch@gmail.com>
2023-07-18 08:16:13 +0900

    fix: Fix typo in class name and method names

--- src/npc_classifier/npc_classifier.py
+++ src/npc_classifier/npc_classifier.py
@@ -5,76 +5,87 @@ import numpy as np
 from collections import defaultdict
 import operator
 from joblib import Parallel, delayed
+from typing import cast
 
 
-class DefaultCompressor:
+class ZlibCompressor:
     def __init__(self):
         pass
 
-    def get_compressed_len(self, text: str) -> int:
+    def compress_and_get_length(self, text: str) -> int:
         return len(zlib.compress(text.encode("utf-8")))
 
 
-def agg_func(text1: str, text2: str) -> str:
+def combine_texts(text1: str, text2: str) -> str:
     return text1 + text2
 
 
-def dis_func(len1: int, len2: int, len12: int) -> float:
-    return (len12 - min(len1, len2)) / max(len1, len2)
+def calculate_normalized_distance(len1: int, len2: int, combined_len: int) -> float:
+    return (combined_len - min(len1, len2)) / max(len1, len2)
 
 
 class NPCClassifier(BaseEstimator, ClassifierMixin):
     def __init__(
         self,
-        agg_func: Callable[[str, str], str] = agg_func,
-        dis_func: Callable[[int, int, int], float] = dis_func,
+        combine_texts: Callable[[str, str], str] = combine_texts,
+        calculate_distance: Callable[
+            [int, int, int], float
+        ] = calculate_normalized_distance,
         k: int = 2,
         n_jobs: int = 1,
     ):
-        self.agg_func = agg_func
-        self.compressor = DefaultCompressor()
-        self.dis_func = dis_func
+        self.combine_texts = combine_texts
+        self.compressor = ZlibCompressor()
+        self.calculate_distance = calculate_distance
         self.k = k
         self.n_jobs = n_jobs
-        self.train_data = None
-        self.train_label = None
-        self.train_compressed = None
+        self.train_texts = []
+        self.train_labels = []
+        self.compressed_train_texts = []
 
     def fit(self, X: List[str], y: List[int]) -> "NPCClassifier":
-        self.train_data = X
-        self.train_label = y
-        self.train_compressed = Parallel(n_jobs=self.n_jobs)(
-            delayed(self.compressor.get_compressed_len)(x) for x in X
+        self.train_texts = X
+        self.train_labels = y
+        compressed_train_texts = Parallel(n_jobs=self.n_jobs)(
+            delayed(self.compressor.compress_and_get_length)(x) for x in X
         )
+        self.compressed_train_texts = cast(List[int], compressed_train_texts)
         return self
 
     def predict(self, X: List[str]) -> List[int]:
-        y_pred = Parallel(n_jobs=self.n_jobs)(
+        predicted_labels = Parallel(n_jobs=self.n_jobs)(
             delayed(self.predict_single)(x) for x in X
         )
-        return y_pred
+        predicted_labels = cast(List[int], predicted_labels)
+        return predicted_labels
 
-    def predict_single(self, x: str) -> int:
-        distance4i = self.calc_dis_single_multi(self.train_data, x)
-        sorted_idx = np.argsort(np.array(distance4i))
-        pred_labels = defaultdict(int)
+    def predict_single(self, text: str) -> int:
+        distances = self.calculate_distances_to_train_texts(self.train_texts, text)
+        sorted_indices = np.argsort(np.array(distances))
+        label_counts = defaultdict(int)
         for j in range(self.k):
-            pred_l = self.train_label[sorted_idx[j]]
-            pred_labels[pred_l] += 1
-        sorted_pred_lab = sorted(
-            pred_labels.items(), key=operator.itemgetter(1), reverse=True
+            nearest_label = self.train_labels[sorted_indices[j]]
+            label_counts[nearest_label] += 1
+        sorted_label_counts = sorted(
+            label_counts.items(), key=operator.itemgetter(1), reverse=True
         )
-        most_label = sorted_pred_lab[0][0]
-        return most_label
+        most_frequent_label = sorted_label_counts[0][0]
+        return most_frequent_label
 
-    def calc_dis_single_multi(self, train_data: List[str], datum: str) -> List[float]:
-        distance4i = []
-        t1_compressed = self.compressor.get_compressed_len(datum)
-        for j, t2 in enumerate(train_data):
-            t2_compressed = self.train_compressed[j]
-            t1t2_compressed = self.compressor.get_compressed_len(
-                self.agg_func(datum, t2)
+    def calculate_distances_to_train_texts(
+        self, train_texts: List[str], test_text: str
+    ) -> List[float]:
+        distances = []
+        test_text_compressed_len = self.compressor.compress_and_get_length(test_text)
+        for j, train_text in enumerate(train_texts):
+            train_text_compressed_len = self.compressed_train_texts[j]
+            combined_text_compressed_len = self.compressor.compress_and_get_length(
+                self.combine_texts(test_text, train_text)
             )
-            distance = self.dis_func(t1_compressed, t2_compressed, t1t2_compressed)
-            distance4i.append(distance)
-        return distance4i
+            distance = self.calculate_distance(
+                test_text_compressed_len,
+                train_text_compressed_len,
+                combined_text_compressed_len,
+            )
+            distances.append(distance)
+        return distances
