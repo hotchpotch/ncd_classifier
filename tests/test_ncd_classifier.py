04ae66f822fee4a19def6202f4ec08d9071af584
Yuichi Tateno <hotchpotch@gmail.com>
2023-07-21 05:26:13 +0900

    test_ncd_classifier_all_comporessors: change y_text to y_test

--- tests/test_ncd_classifier.py
+++ tests/test_ncd_classifier.py
@@ -3,49 +3,49 @@ from sklearn.metrics import accuracy_score
 
 
 def get_data():
-    X_train = ["hello world", "hello", "world", "what's up?"]
+    X_train = ["hello world", "hello?", "world?", "good evening"]
     y_train = [0, 0, 0, 1]
     X_test = ["hello", "world"]
-    y_text = [0, 0]
-    return X_train, y_train, X_test, y_text
+    y_test = [0, 0]
+    return X_train, y_train, X_test, y_test
 
 
 def get_list_int_data():
     X_train = [[1, 2, 3], [1, 2], [1, 2, 3, 4], [100, 200, 300, 400, 500]]
     y_train = [0, 0, 0, 1]
     X_test = [[1, 2], [1, 2, 3, 4]]
-    y_text = [0, 0]
-    return X_train, y_train, X_test, y_text
+    y_test = [0, 0]
+    return X_train, y_train, X_test, y_test
 
 
 def test_ncd_classifier():
-    X_train, y_train, X_test, y_text = get_data()
+    X_train, y_train, X_test, y_test = get_data()
 
     classifier = NCDClassifier(n_jobs=2, k=3)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
 
-    assert accuracy_score(y_text, y_pred) == 1
+    assert accuracy_score(y_test, y_pred) == 1
 
     assert len(y_pred) == len(X_test)
     assert all(isinstance(y, int) for y in y_pred)
 
 
 def test_ncd_classifier_list_int():
-    X_train, y_train, X_test, y_text = get_list_int_data()
+    X_train, y_train, X_test, y_test = get_list_int_data()
 
     classifier = NCDClassifier(n_jobs=2, k=3)
     classifier.fit(X_train, y_train)
     y_pred = classifier.predict(X_test)
 
-    assert accuracy_score(y_text, y_pred) == 1
+    assert accuracy_score(y_test, y_pred) == 1
 
     assert len(y_pred) == len(X_test)
     assert all(isinstance(y, int) for y in y_pred)
 
 
 def test_ncd_classifier_all_comporessors():
-    X_train, y_train, X_test, y_text = get_data()
+    X_train, y_train, X_test, y_test = get_data()
 
     for name, fn in COMPRESSORS.items():
         classifier = NCDClassifier(n_jobs=2, k=3, compress_len_fn=fn)
@@ -55,6 +55,6 @@ def test_ncd_classifier_all_comporessors():
         if name == "lzma":
             # skip
             continue
-        assert accuracy_score(y_text, y_pred) == 1
+        assert accuracy_score(y_test, y_pred) == 1
         assert len(y_pred) == len(X_test)
         assert all(isinstance(y, int) for y in y_pred)
